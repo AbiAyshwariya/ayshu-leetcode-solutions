3e29521b5d7cef8c34e3f20bac1fef435b6cd059
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2025-10-03 23:12:35 +0530

    Time: 0 ms (100%) , Space: 42.1 MB (48.27%), Difficulty: Medium

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,18 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        int low=0;
-        int mid=0;
-        int high=n-1;
-        while(mid<=high){
-            if(nums[mid]==0){
-                int temp=nums[low];
-                nums[low]=nums[mid];
-                nums[mid]=temp;
-                mid++;
-                low++;
-            }
-            else if(nums[mid]==1){
-                mid++;
-            }
-            else{
-                int temp=nums[mid];
-                nums[mid]=nums[high];
-                nums[high]=temp;
-                high--;
-            }
+        int count0 = 0, count1 = 0, count2 = 0;
+        
+        // Count occurrences
+        for (int num : nums) {
+            if (num == 0) count0++;
+            else if (num == 1) count1++;
+            else count2++;
         }
+        
+        // Fill back into nums
+        int index = 0;
+        while (count0-- > 0) nums[index++] = 0;
+        while (count1-- > 0) nums[index++] = 1;
+        while (count2-- > 0) nums[index++] = 2;
     }
 }
