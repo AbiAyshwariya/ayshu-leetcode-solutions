897dc46d74885d95adc609c4cf6c7a0caf9fb484
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2025-11-23 16:43:44 +0530

    Time: 3 ms (68.88%) , Space: 43.3 MB (14.42%), Difficulty: Easy

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,40 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> s1=new Stack<>();
+        boolean result=false;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='{' || ch=='[' || ch=='('){
+                s1.push(ch);
+            }
+            else {
+                
+                if(s1.isEmpty()){
+                    return false;
+                }
+                else{
+
+                    char ans=s1.peek();
+                  if(ans=='(' && ch==')'){
+                    s1.pop();
+                  }
+                  else if(ans=='['&& ch==']'){
+                    s1.pop();
+                  }
+                  else if(ans=='{' && ch=='}'){
+                    s1.pop();
+                  }
+                  else{
+                    return false;
+                  }
+                }
+            }
+        }
+        if(!s1.isEmpty()){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+}
