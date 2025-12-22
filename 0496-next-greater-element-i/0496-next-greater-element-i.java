eefe2acba2c604aac26a161b7f583f8f1e9f8a29
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2025-12-22 17:51:19 +0530

    Time: 207 ms (6.05%) , Space: 47.1 MB (7.34%), Difficulty: Easy

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,28 +1,36 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-        Stack<Integer> st = new Stack<>();
-        Map<Integer,Integer> mpp=new HashMap<>();
-        for(int i=n2-1;i>=0;i--){
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int[] nge=new int[n1];
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(nums1[i]==nums2[j]){
+                    nge[i]=findNextGreater(nums2,j);
+
+                }
+            }
+        }
+        return nge;
+    }
+
+    public int findNextGreater(int[] nums2,int j){
+        Stack<Integer> st=new Stack<>();
+        List<Integer> list=new ArrayList<>();
+        int n2=nums2.length;
+        for(int i=n2-1;i>=j;i--){
             while(!st.isEmpty() && st.peek()<=nums2[i]){
                 st.pop();
             }
             if(st.isEmpty()){
-                mpp.put(nums2[i],-1);
+               list.add(-1);
             }
             else{
-                mpp.put(nums2[i],st.peek());
+                list.add(st.peek());
             }
             st.push(nums2[i]);
         }
-
-        int[] nge=new int[n1];
-        for(int i=0;i<n1;i++){
-            nge[i]=mpp.get(nums1[i]);
-        }
-
-        return nge;
+        
+        return list.get(list.size()-1);
     }
-
 }
