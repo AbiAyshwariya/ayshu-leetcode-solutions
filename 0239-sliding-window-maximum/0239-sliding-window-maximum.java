f0c5a34db161f95077edc611dee5517f4a6f0bd9
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-01-08 15:17:23 +0530

    Time: 36 ms (25.3%) , Space: 161.9 MB (5.1%), Difficulty: Hard

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -2,12 +2,18 @@ class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         int n=nums.length;
         ArrayList<Integer> list=new ArrayList<>();
-        for(int i=0;i<=n-k;i++){
-             int maxi=Integer.MIN_VALUE;
-           for(int j=i;j<i+k;j++){
-              maxi=Math.max(maxi,nums[j]);
-           }
-           list.add(maxi);
+        Deque<Integer> dq=new LinkedList<>();
+        for(int i=0;i<n;i++){
+            if(!dq.isEmpty() && dq.peekFirst()<=i-k){
+                dq.pollFirst();
+            }
+            while(!dq.isEmpty() && nums[dq.peekLast()]<nums[i]){
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if(i>=k-1){
+                list.add(nums[dq.peekFirst()]);
+            }
         }
         int num=list.size();
         int[] arr=new int[num];
