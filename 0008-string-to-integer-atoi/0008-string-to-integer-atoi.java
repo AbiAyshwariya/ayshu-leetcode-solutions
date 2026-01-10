f5dc935bf2682a7c6d7cd3b26a47080d29891dea
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-01-10 14:07:27 +0530

    Time: 2 ms (16.08%) , Space: 43.9 MB (29.03%), Difficulty: Medium

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -1,38 +1,40 @@
 class Solution {
     public int myAtoi(String s) {
         s=s.trim();
-        int index=0;
         if(s.length()==0){
             return 0;
         }
+        int index=0;
         int sign=1;
-        if(s.charAt(index)=='-'){
+        if(s.charAt(0)=='-'){
             sign=-1;
             index++;
         }
-        else if(s.charAt(index)=='+'){
+        else if(s.charAt(0)=='+'){
+            sign=1;
             index++;
         }
 
         long result=helper(s,index,0L);
         result=result*sign;
-        if(result>Integer.MAX_VALUE){return Integer.MAX_VALUE; };
-        if(result<Integer.MIN_VALUE){return Integer.MIN_VALUE; };
+        if(result>Integer.MAX_VALUE){
+            return Integer.MAX_VALUE;
+        }
+        if(result<Integer.MIN_VALUE){
+            return Integer.MIN_VALUE;
+        }
         return (int)result;
-
     }
 
-    public static long helper(String s, int index, long result){
-        if(index>=s.length() || !Character.isDigit(s.charAt(index))){
-            return result;
-        }
-        int digit=s.charAt(index)-'0';
-        result=result*10+digit;
-
-        if (result > (long) Integer.MAX_VALUE + 1) {
-            return result;
-        }
-        return helper(s,index+1,result);
-        
+    public long helper(String s, int index, long result){
+     if(index>=s.length() || !Character.isDigit(s.charAt(index))){
+        return result;
+     }
+     int digit=s.charAt(index)-'0';
+     result=result*10+digit;
+     if(result>Integer.MAX_VALUE){
+        return result;
+     }
+     return helper(s,index+1,result);
     }
 }
