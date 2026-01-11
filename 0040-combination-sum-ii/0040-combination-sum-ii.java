9fb86bacbc34dbef4079dd4f5f8b424591808185
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-01-11 11:20:01 +0530

    Time: 5 ms (98.94%) , Space: 45.1 MB (79.95%), Difficulty: Medium

--- 0040-combination-sum-ii/0040-combination-sum-ii.java
+++ 0040-combination-sum-ii/0040-combination-sum-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans=new ArrayList<>();
+        List<List<Integer>> ls=new ArrayList<>();
+        List<Integer> list=new ArrayList<>();
         Arrays.sort(candidates);
-        findCombinations(candidates,0,target,new ArrayList<>(),ans);
-        return ans;
-    }    
+        generate(candidates,0,target,list,ls);
+        return ls;
+    }
 
-    public static void findCombinations(int[] arr, int ind, int target,List<Integer> list,List<List<Integer>> ans){
-        if(target==0){
-            ans.add(new ArrayList<>(list));
-            return;
-        }
-
+    public void generate(int[] arr,int ind,int target, List<Integer> list,List<List<Integer>> ls){
+         if(target==0){
+                ls.add(new ArrayList<>(list));
+                return;
+            }
         for(int i=ind;i<arr.length;i++){
-            if(i>ind && arr[i]==arr[i-1]){
-                continue;
-            }
-            if(arr[i]>target){
-                break;
-            }
+           if(i>ind && arr[i]==arr[i-1]){
+            continue;
+           }
+           if(arr[i]>target){
+            break;
+           }
+        
             list.add(arr[i]);
-            findCombinations(arr,i+1,target-arr[i],list,ans);
+            generate(arr,i+1,target-arr[i],list,ls);
             list.remove(list.size()-1);
+            
         }
-
-
     }
 }
