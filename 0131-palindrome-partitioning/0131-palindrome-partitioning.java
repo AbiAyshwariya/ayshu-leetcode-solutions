34ad0ddfc626aa0635caba0d799d891b32592954
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-02-07 19:08:57 +0530

    Time: 11 ms (12.97%) , Space: 65.4 MB (37.35%), Difficulty: Medium

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,33 +1,34 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        int n=s.length();
         List<List<String>> ls=new ArrayList<>();
-        partition(s,n,ls,new ArrayList<>(),0);
+        generate(s,ls,new ArrayList<>(),0);
         return ls;
     }
 
-    public void partition(String s, int n, List<List<String>> ls, List<String> arr,int ind){
-        if(ind==n){
+    public void generate(String str, List<List<String>> ls, List<String> arr,int ind){
+        if(ind==str.length()){
             ls.add(new ArrayList<>(arr));
             return;
         }
-        for(int i=ind;i<n;i++){
-            if(checkPalindrome(s,ind,i)){
-                arr.add(s.substring(ind,i+1));
-                partition(s,n,ls,arr,i+1);
+
+        for(int i=ind;i<str.length();i++){
+            if(checkPalindrome(str,ind,i)){
+                arr.add(str.substring(ind,i+1));
+                generate(str,ls,arr,i+1);
                 arr.remove(arr.size()-1);
             }
         }
     }
 
-    public boolean checkPalindrome(String s, int start, int end){
-         while(start < end){
-        if(s.charAt(start) != s.charAt(end)){
-            return false;
+    public boolean checkPalindrome(String str, int start, int end){
+        while(start<end){
+            if(str.charAt(start)!=str.charAt(end)){
+                return false;
+            }
+            start++;
+            end--;
         }
-        start++;
-        end--;
+        return true;
     }
-    return true;
-    }
+
 }
