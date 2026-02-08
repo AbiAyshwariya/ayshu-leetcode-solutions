7100f38a0fbeb7bd76103756214995917aee0593
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-02-08 17:16:48 +0530

    Time: 128 ms (87.54%) , Space: 43.2 MB (42.84%), Difficulty: Medium

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,39 +1,41 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int n=board.length;
-        int m=board[0].length;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j] == word.charAt(0)){
+       int n=board.length;
+       int m=board[0].length;
+       for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            if(word.charAt(0)==board[i][j]){
                 if(dfs(board,word,i,j,0)){
                     return true;
                 }
-                }
             }
         }
-        return false;
+       } 
+       return false;
     }
 
-    public boolean dfs(char[][] board,String word,int i,int j,int idx){
-        if(idx==word.length()){
+    public boolean dfs(char[][] board,String word, int i,int j, int idz){
+        if(idz==word.length()){
             return true;
         }
 
-        if(i<0||j<0||j>=board[0].length||i>=board.length){
+        if(i<0||j<0||i>=board.length||j>=board[0].length){
             return false;
         }
 
-        if (board[i][j] != word.charAt(idx)) {
-            return false;
+        if(board[i][j]!=word.charAt(idz)){
+           return false;
         }
+
         char temp=board[i][j];
         board[i][j]='#';
-        boolean found=dfs(board,word,i+1,j,idx+1)||
-                      dfs(board,word,i-1,j,idx+1)||
-                      dfs(board,word,i,j-1,idx+1)||
-                      dfs(board,word,i,j+1,idx+1);
-        board[i][j]=temp;
-        return found;
 
+        boolean found=dfs(board,word,i+1,j,idz+1)||
+                      dfs(board,word,i-1,j,idz+1)||
+                      dfs(board,word,i,j+1,idz+1)||
+                      dfs(board,word,i,j-1,idz+1);
+        board[i][j]=temp;
+
+        return found;
     }
 }
