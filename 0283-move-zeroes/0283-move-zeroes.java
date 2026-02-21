3406999318137b020ad1ec3e8fd7f331f46ed8b6
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-02-21 14:31:03 +0530

    Time: 2 ms (90.09%) , Space: 47.7 MB (76.56%), Difficulty: Easy

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,20 +1,24 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int n=nums.length;
-        int j;
-        for(j=0;j<n;j++){
-            if(nums[j]==0){
-                break;
-            }
+       int n=nums.length;
+       int j=-1;
+       for(int i=0;i<n;i++){
+        if(nums[i]==0){
+            j=i;
+            break;
         }
+       } 
+       if(j==-1){
+        return;
+       }
 
-        for(int i=j+1;i<n;i++){
-            if(nums[i]!=0){
-                int temp=nums[i];
-                nums[i]=nums[j];
-                nums[j]=temp;
-                j++;
-            }
-        }
+       for(int i=j;i<n;i++){
+          if(nums[i]!=0){
+            int temp=nums[i];
+            nums[i]=nums[j];
+            nums[j]=temp;
+            j++;
+          }
+       }
     }
 }
