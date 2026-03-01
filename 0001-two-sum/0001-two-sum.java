d38e7a7325619823242b35c438feac651a51a772
Abi Ayshwariya <112408307+AbiAyshwariya@users.noreply.github.com>
2026-03-01 20:44:39 +0530

    Time: 2 ms (99.21%) , Space: 47.3 MB (21.43%), Difficulty: Easy

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,15 +1,15 @@
-import java.util.HashMap;
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-       int n=nums.length;
-       HashMap<Integer,Integer> mpp=new HashMap<>();
-       for(int i=0;i<n;i++){
-        int complement=target-nums[i];
-        if(mpp.containsKey(complement)){
-            return new int[]{i,mpp.get(complement)};
+        int n=nums.length;
+        HashMap<Integer,Integer> mpp=new HashMap<>();
+        int sum=0;
+        for(int i=0;i<n;i++){
+           int complement=target-nums[i];
+           if(mpp.containsKey(complement)){
+                return new int[]{mpp.get(complement),i};
+           }
+           mpp.put(nums[i],i);
         }
-        mpp.put(nums[i],i);
-       }
-       return new int[]{-1,-1};
+        return new int[]{-1,-1};
     }
 }
